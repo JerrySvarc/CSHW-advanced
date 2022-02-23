ea162b35f429b5edd80a5fa6957cc655b07c9a17
Jaroslav Švarc <jara.svarc@email.cz>
2022-02-23 20:29:43 +0100

    Part one of HW1 done

--- 01-Physics-kostra/GamePhysics/Program.cs
+++ 01-Physics-kostra/GamePhysics/Program.cs
@@ -2,7 +2,33 @@
 
 namespace GamePhysics {
 
+
+	struct Seconds
+    {
+		public float value;
+    }
+
+	struct Meters 
+    {
+		public float value;
+		public static Speed operator /(Meters meters, Seconds seconds) => new Speed { value = meters.value / seconds.value };
+	}
+
+
+	struct Speed
+    {
+		public float value;
+		public override string ToString() =>  value + " m/s";
+        
+	}
+
+	static class IntExt
+    {
+        public static Seconds Seconds(this int val) => new Seconds { value = val };
+		public static Meters Meters(this int val) => new Meters { value = val };
+	}
 	class Program {
+		
 		static void Main(string[] args) {
 			var distance = 2.Meters();
 			var time = 3.Seconds();
