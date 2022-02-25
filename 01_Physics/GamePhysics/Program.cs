230993877ec93120dd1769d827fe957a09bff7b7
Jaroslav Švarc <jara.svarc@email.cz>
2022-02-25 23:52:45 +0100

    HW1 finished.

---
+++ 01_Physics/GamePhysics/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace GamePhysics
+{
+    public struct Seconds
+    {
+        public float Value;
+    }
+
+    public struct Meters
+    {
+        public float Value;
+        public static Speed operator /(Meters meters, Seconds seconds) => new Speed { Value = meters.Value / seconds.Value };
+    }
+
+    public struct Speed
+    {
+        public float Value;
+        public override string ToString() => Value + " m/s";
+
+    }
+
+    public static class IntExt
+    {
+        public static Seconds Seconds(this int val) => new Seconds { Value = val };
+        public static Meters Meters(this int val) => new Meters { Value = val };
+        public static Speed MeterPerSeconds(this int val) => new Speed { Value = val };
+    }
+
+    public static class DoubleExt
+    {
+        public static Seconds Seconds(this double val) => new Seconds { Value = (float)val };
+
+    }
+
+    public static class FloatExt
+    {
+        public static Meters Meters(this float val) => new Meters { Value = val };
+        public static Speed MeterPerSeconds(this float val) => new Speed { Value = val };
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var distance = 2.Meters();
+            var time = 3.Seconds();
+            var speed = distance / time;
+            Console.WriteLine($"Speed: {speed}"); // Prints 'Speed: 0.6666666666666666 m/s'
+        }
+    }
+}
---
+++ 01_Physics/JumpingPlatformGame/Entities.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Drawing;
+using GamePhysics;
+
+namespace JumpingPlatformGame {
+
+	struct WorldPoint
+    {
+		public Meters X;
+		public Meters Y;
+	}
+
+	struct Heading
+    {
+		public Meters LowerBound;
+		public Meters UpperBound;
+		public Speed Speed;
+    }
+	
+	abstract class Entity {
+		public virtual Color Color => Color.Black;
+		public WorldPoint Location;
+		public Heading Horizontal;
+		public Heading Vertical;
+
+		public abstract void Update(Seconds deltaSeconds);
+    }
+
+	class MovableEntity : Entity {
+
+		public override void Update(Seconds deltaSeconds)
+		{
+			Meters newLocationX = (Location.X.Value + (deltaSeconds.Value * Horizontal.Speed.Value)).Meters();
+
+            if (newLocationX.Value < Horizontal.LowerBound.Value)
+            {
+				Location.X.Value = Horizontal.LowerBound.Value;
+				Horizontal.Speed.Value *= -1;
+			}
+			else if (newLocationX.Value > Horizontal.UpperBound.Value)
+            {
+				Location.X.Value = Horizontal.UpperBound.Value;
+				Horizontal.Speed.Value *= -1;
+			}
+            else
+            {
+				Location.X = newLocationX;
+            }
+		}
+	}
+
+	class MovableJumpingEntity : MovableEntity {
+
+
+		public override void Update(Seconds deltaSeconds)
+		{
+			Meters newLocationX = (Location.X.Value + (deltaSeconds.Value * Horizontal.Speed.Value)).Meters();
+
+			if (newLocationX.Value < Horizontal.LowerBound.Value)
+			{
+				Location.X.Value = Horizontal.LowerBound.Value;
+				Horizontal.Speed.Value *= -1;
+			}
+			else if (newLocationX.Value > Horizontal.UpperBound.Value)
+			{
+				Location.X.Value = Horizontal.UpperBound.Value;
+				Horizontal.Speed.Value *= -1;
+			}
+			else
+			{
+				Location.X = newLocationX;
+			}
+
+			Meters newLocationY = (Location.Y.Value + (deltaSeconds.Value * Vertical.Speed.Value)).Meters();
+			
+			if (newLocationY.Value < Vertical.LowerBound.Value)
+			{
+				Location.Y.Value = Vertical.LowerBound.Value;
+			}
+			else if (newLocationY.Value > Vertical.UpperBound.Value)
+			{
+				Location.Y.Value = Vertical.UpperBound.Value;
+				Vertical.Speed.Value *= -1;
+			}
+			else
+			{
+				Location.Y = newLocationY;
+			}
+		}
+	}
+
+	class Joe : MovableEntity {
+		public override string ToString() => "Joe";
+		public override Color Color => Color.Blue;
+	}
+
+	class Jack : MovableEntity {
+		public override string ToString() => "Jack";
+		public override Color Color => Color.LightBlue;
+	}
+
+	class Jane : MovableJumpingEntity {
+		public override string ToString() => "Jane";
+		public override Color Color => Color.Red;
+	}
+
+	class Jill : MovableJumpingEntity {
+		public override string ToString() => "Jill";
+		public override Color Color => Color.Pink;
+	}
+
+}
